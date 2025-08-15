f8f64a003127e5a46c0571738dd3b17ad14e99ab
sal <sal@sals-MacBook-Pro.local>
2025-08-15 10:24:01 -0300

    feat: add login page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+import { pattern } from "motion/react-client";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ik.imagekit.io",
+        protocol: "https",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
