e403f4f27fc7097c01bb012083558ec146aafdba
sal <sal@sals-MBP.fibertel.com.ar>
2025-08-18 21:52:08 -0300

    fix: more product page errors

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -2,8 +2,8 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.UPABASE_ANON_KEY;
 
 export const createClient = async () => {
   const cookieStore = await cookies();
