1f94cfd1467ac99b6e95810863ba18ba4201f947
SebastianLezama <sebastian.lezama@gmail.com>
2025-11-04 19:53:50 -0300

    fix: var not used errors

--- src/components/sign-up.tsx
+++ src/components/sign-up.tsx
@@ -1,4 +1,4 @@
-import { Logo, LogoIcon, LogoString } from '@/components/logo'
+// import { Logo, LogoIcon, LogoString } from '@/components/logo'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
--- src/lib/actions/mp.actions.ts
+++ src/lib/actions/mp.actions.ts
@@ -1,10 +1,10 @@
-import { createClient } from "@/utils/supabase/server";
+// import { createClient } from "@/utils/supabase/server";
 import { CreateOrder } from "@/utils/utils";
 
 export const createOrder = async (formData: CreateOrder) => {
-  const supabase = await createClient();
-  const email = (await (supabase).auth.getUser()).data.user?.email;
+  // const supabase = await createClient();
+  // const email = (await (supabase).auth.getUser()).data.user?.email;
 
-  const { data, error } = await supabase.from('profiles').select('id').eq('email', email).single();
+  // const { data, error } = await supabase.from('profiles').select('id').eq('email', email).single();
 
 }
